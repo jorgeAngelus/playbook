8f522ba86e7259774b641eb26cdac7f9c2721c9f
Jorge MC <angelus.tux@gmail.com>
2022-04-13 03:22:47 -0500

    realicé ejemplos con example7

--- weekly_mission_1/example7/main_2.js
+++ weekly_mission_1/example7/main_2.js
@@ -1,3 +1,11 @@
-import { log } from './logger.js'
+/*import { log } from './logger.js'*/
+import { Logger,LEVELS } from './logger.js'  
+//importa alguno de los elementos definidos en logger.js *Logger es la clase 
+// y LEVELS es un objeto
 
-log('Hello world')
+//log('Hello world')
+
+const objectClass = new Logger("ClaseLogger")
+objectClass.log("primer mensaje")
+
+console.log(LEVELS.info)
