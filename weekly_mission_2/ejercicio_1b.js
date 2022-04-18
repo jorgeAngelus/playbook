146d3524158ff92656f406ddfdcba81eb8bd96b0
Jorge MC <angelus.tux@gmail.com>
2022-04-18 04:00:13 -0500

    modelando twitter

---
+++ weekly_mission_2/ejercicio_1b.js
@@ -0,0 +1,23 @@
+const Twitter ={
+    perfil = {
+        username: "jorge0710",
+        email: "jmc@outlook.com",
+        siguiendo: 256,
+        seguidores: 13,
+        pais: "México",
+        fechaunion: "julio 2016",
+    },
+
+    comentario = {
+        texto:"follow me in ....",
+        user: perfil.username 
+    },
+
+    trending_topic ={
+        text:"#FirstLady",
+        numTwits: "45 mil"
+    },
+    hashtag: "#IloveJS"
+}
+
+console.log(Twitter.hashtag)
