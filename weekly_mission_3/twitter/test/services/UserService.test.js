7a7960f0290a2dbd04ff1078544e32cad460d1c4
Jorge MC <angelus.tux@gmail.com>
2022-04-25 01:21:53 -0500

    se agregaron getter y setters; y la funcion Get Info para Userservice

--- weekly_mission_3/twitter/test/services/UserService.test.js
+++ weekly_mission_3/twitter/test/services/UserService.test.js
@@ -2,10 +2,26 @@ const UserService = require('./../../app/services/UserService')
 
 describe("Test for UserService",() =>{
     test("1. Create a new user using the UserService",() =>{
-        const user = UserService.Create(1,"jorgemc","Jorge")
+        const user = UserService.create(1,"jorgemc","Jorge")
         expect(user.username).toBe("jorgemc")
         expect(user.name).toBe("Jorge")
         expect(user.id).toBe(1)
         expect(user.bio).not.toBeUndefined()
     });
+
+    test("2. Get all user data in a list",() =>{
+        const user = UserService.create(1,"jorgemc07","Jorge")
+        const userInfoInList = UserService.getInfo(user)
+
+        expect(userInfoInList[0]).toBe(1)
+        expect(userInfoInList[1]).toBe("jorgemc07")
+        expect(userInfoInList[2]).toBe("Jorge")
+        expect(userInfoInList[3]).toBe("Sin bio")
+    });
+
+    /*test("3 Update username",() =>{
+        const user = UserService.create(1,"jorgemc07","Jorge")
+        UserService.updateUsername(user,"jorgemc07")
+        expect(user.username).toBe("jorgemc07")
+    })*/
 })
