db05f47c2e4fa9c175b1eadb84825a5e18667a3e
Jorge MC <angelus.tux@gmail.com>
2022-04-24 20:58:54 -0500

    Cambio en user test

--- weekly_mission_3/twitter/test/models/User.test.js
+++ weekly_mission_3/twitter/test/models/User.test.js
@@ -3,14 +3,14 @@ const User = require("./../../app/models/User")
 describe("Unit Test for User Class",() =>{
     test('Create an User object',() =>{
         //codigo de la app
-        const user = new User(1,"jorgemc07","Jorge","Bio","dateCreated","lastUpdate")
+        const user = new User(1,"jorgemc07","Jorge","Bio")
 
         //validando resultados
         expect(user.id).toBe(1)
         expect(user.username).toBe("jorgemc07")
         expect(user.name).toBe("Jorge")
         expect(user.bio).toBe("Bio")
-        expect(user.dateCreated).toBe("dateCreated")
-        expect(user.lastUpdate).toBe("lastUpdate")
+        expect(user.dateCreated).toBeUndefined("dateCreated")
+        expect(user.lastUpdate).toBeUndefined("lastUpdate")
     });
 })
